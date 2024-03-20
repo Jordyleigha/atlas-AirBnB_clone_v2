b65353105e2ad6e15b4dcd539f6244bdf2b91b5f
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2024-03-20 11:12:24 -0500

    Update 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
-'''This module starts a Flask web app'''
+"""
+starts a Flask web application
+"""
 
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
+def index():
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
+def hbnb():
+    """returns HBNB"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def display_text(text):
-    formatted_text = text.replace('_', ' ')
-    return f'C {formatted_text}'
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
